6e34ba478e3143ad463f5e81b2551c47fea18910
JEdgarBaldelomarSalazar <52173749+JEdgarBaldelomarSalazar@users.noreply.github.com>
2024-04-12 19:23:04 +0200

    reformat file

--- source/main.py
+++ source/main.py
@@ -4,8 +4,10 @@ from selenium.webdriver.chrome.options import Options
 from ProcessPage import ProcessPage
 from GetRobotsFile import GetRobotsFile
 import pandas as pd
+
+
 def main() -> int:
-    #driver = get_web_driver()
+    driver = get_web_driver()
     robotsFiles = GetRobotsFile()
     robotsFiles.get_robots_file()
     process_page = ProcessPage(driver)
@@ -13,12 +15,14 @@ def main() -> int:
     driver.close()
     return 0
 
+
 def get_web_driver() -> webdriver:
     options = Options()
     user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
     options.add_argument(f'--user-agent={user_agent}')
     driver = webdriver.Chrome()
     return driver
+
 
 if __name__ == "__main__":
     sys.exit(main())
