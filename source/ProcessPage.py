9a08182d28203fe04c346f13af6f183a7aab07b5
JEdgarBaldelomarSalazar <52173749+JEdgarBaldelomarSalazar@users.noreply.github.com>
2024-04-12 20:11:04 +0200

    enable click, done, pending handle next page

--- source/ProcessPage.py
+++ source/ProcessPage.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 from Constants import *
+
 class ProcessPage:
 
     def __init__(self, webdriver):
@@ -39,17 +41,13 @@ class ProcessPage:
     def select_option(self):
         try:
             select_element = self.webdriver.find_element(By.XPATH, self.root_xpath)
+            select = Select(select_element)
             for key in self.materiales:
-                xpath = f'./option[@value="{key}"]'
-                select_option = select_element.find_element(By.XPATH,  xpath)
-                select_option.click()
+                select.select_by_value(key)
                 iframe = self.webdriver.find_element(By.ID, btn_cosultar_seleccion_id)
                 ActionChains(self.webdriver).scroll_to_element(iframe).perform()
                 button = self.webdriver.find_element(By.ID, btn_cosultar_seleccion_id)
                 button.click()
-                content = WebDriverWait(self.webdriver, 5).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, 'sd')))
-                print(content.text)
         except NoSuchElementException as e:
             print(f'Element not found {e}')
         finally:
