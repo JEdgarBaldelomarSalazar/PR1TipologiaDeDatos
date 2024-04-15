d8ae1be6dd90cf8efdf4eb54dcf46f240c6a2d5a
JEdgarBaldelomarSalazar <52173749+JEdgarBaldelomarSalazar@users.noreply.github.com>
2024-04-15 20:09:14 +0200

    process table data from each material, done --> pending create csv

--- source/ProcessPage.py
+++ source/ProcessPage.py
@@ -20,8 +20,9 @@ class ProcessPage:
         self.data_links = {}
 
     def process_page(self) -> None:
-        self.get_materiales()
-        self.select_option()
+        #self.get_materiales()
+        #self.select_option()
+        self.get_data()
 
     def get_materiales(self):
         self.webdriver.implicitly_wait(2)
@@ -77,6 +78,55 @@ class ProcessPage:
         except StaleElementReferenceException:
             attempts += 1
             time.sleep(1)
+        except NoSuchElementException as e:
+            print(f'Element not found {e}')
+        finally:
+            self.webdriver.quit()
+
+    def get_data(self):
+        links = {
+            "Aluminio": "https://www.ine.es/consul/serie.do?d=true&s=IMM32",
+            "Materiales bituminosos": "https://www.ine.es/consul/serie.do?d=true&s=IMM55",
+            "Cemento": "https://www.ine.es/consul/serie.do?d=true&s=IMM30",
+            "Energía": "https://www.ine.es/consul/serie.do?d=true&s=IMM29",
+            "Focos y luminarias": "https://www.ine.es/consul/serie.do?d=true&s=IMM28",
+            "Materiales cerámicos": "https://www.ine.es/consul/serie.do?d=true&s=IMM27",
+            "Madera": "https://www.ine.es/consul/serie.do?d=true&s=IMM26",
+            "Productos plásticos": "https://www.ine.es/consul/serie.do?d=true&s=IMM25",
+            "Productos quimicos": "https://www.ine.es/consul/serie.do?d=true&s=IMM24",
+            "Aridos y rocas": "https://www.ine.es/consul/serie.do?d=true&s=IMM23",
+            "Materiales siderúrgicos": "https://www.ine.es/consul/serie.do?d=true&s=IMM54",
+            "Materiales electrónicos": "https://www.ine.es/consul/serie.do?d=true&s=IMM21",
+            "Cobre": "https://www.ine.es/consul/serie.do?d=true&s=IMM20",
+            "Vidrio": "https://www.ine.es/consul/serie.do?d=true&s=IMM19",
+            "Materiales explosivos": "https://www.ine.es/consul/serie.do?d=true&s=IMM18"
+        }
+        #for key, link in self.data_links.items():
+        try:
+            for key, link in links.items():
+                self.webdriver.get(f'{link}')
+                self.webdriver.implicitly_wait(2)
+                disable_item = self.webdriver.find_element(By.XPATH, "//option[@value='15']")
+                self.webdriver.execute_script("arguments[0].removeAttribute('selected')", disable_item)
+                self.webdriver.implicitly_wait(1)
+                select_form = self.webdriver.find_element(By.XPATH, "//div[@id='a']//select[@class='menuSelect']")
+                select = Select(select_form)
+                select.select_by_value("100")
+                self.webdriver.implicitly_wait(1)
+                submit_btn = self.webdriver.find_element(By.XPATH, "//div[@id='a']//input[@class='botonIr']")
+                submit_btn.click()
+                time.sleep(5)
+                table_element = self.webdriver.find_element(By.XPATH, "//table[@class='general']/tbody")
+                rows = table_element.find_elements(By.TAG_NAME, "tr")
+                for row in rows:
+                    cells = row.find_elements(By.XPATH, "./th | ./td")
+                    for index, cell in enumerate(cells):
+                        print(cell.text)
+                        if index == 1:
+                            break
+            self.webdriver.close()
+        except StaleElementReferenceException:
+            print("stale")
         except NoSuchElementException as e:
             print(f'Element not found {e}')
         finally:
